ff26ea73687d1c437e6b7d89bfbb7f090e80a892
Andres <andresvz50@gmail.com>
2026-01-31 21:57:37 -0500

    updated cypress to latest version

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "cypress";
+import installLogsPrinter from 'cypress-terminal-report/src/installLogsPrinter';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      installLogsPrinter(on, {
+        printLogsToConsole: 'onFail',
+        printLogsToFile: 'always',
+        outputRoot: 'reports/terminal'
+      });
     },
+    reporter: 'junit',
+    reporterOptions: {
+      mochaFile: 'reports/junit/[name]-[hash].xml',
+      toConsole: false,
+      attachments: true
+    }
   },
 });
